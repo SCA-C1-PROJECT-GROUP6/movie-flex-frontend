10873b9213d7709efda03fc1d92c1968682f0a86
Dabi-ayu <dabiayu8@gmail.com>
2024-06-12 22:07:07 +0100

    clearified code

--- movie-flex-frontend/src/pages/SingUp.jsx
+++ movie-flex-frontend/src/pages/SingUp.jsx
@@ -1,8 +1,6 @@
 import { useState } from "react";
 import SignUpValidator from "../validators/SignUpValidator";
 import "./SignUp.css";
-import Navbar from "../components/Navbar";
-import MainNavbar from "../components/SubNavbar";
 
 const SignUp = () => {
   const eyeIconUrl = "https://i.imgur.com/nW2iNQm.png";
@@ -48,10 +46,8 @@ const SignUp = () => {
 
   return (
     <>
-    <Navbar/>
-    <MainNavbar />
       <div className="ml-24 md:ml-1 lg:ml-20">
-        <img src={logoUrl} alt="logo img" className="w-48 md:w-24 lg:w-40" />
+        <img src={logoUrl} alt="logo img" className="w-48 md:w-24 lg:w-40 -mt-6" />
       </div>
       <div className=" p-4 -mt-24 lg:-mt-8 max-w-xl mx-auto rounded-md wrapper text-white h-199 shadow-lg z-10 backdrop-filter backdrop-blur-sm md:mt-2">
         <h1 className="text-xl font-bold ">
