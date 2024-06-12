ca453ce3243f9b9a5fc5e5821426c74821227279
Dabi-ayu <dabiayu8@gmail.com>
2024-06-12 12:44:22 +0100

    structure of signup updated

--- movie-flex-frontend/src/pages/SingUp.jsx
+++ movie-flex-frontend/src/pages/SingUp.jsx
@@ -1,20 +1,24 @@
 import "./SignUp.css";
-import eyeIcon from "./assets/eyeIcon.png";
-import GIcon from "./assets/Google.png";
-import Facebook from "./assets/Facebook.png";
 
 const SignUp = () => {
+  const eyeIconUrl = "https://i.imgur.com/nW2iNQm.png";
+  const GIconUrl = "https://i.imgur.com/GJnmeaj.png";
+  const FbIconUrl = "https://i.imgur.com/QFwv2qR.png";
+  const logoUrl = "https://i.imgur.com/ouyiPCG.png";
   return (
     <>
-      <div className=" p-4 shadow-3xl mt-12 max-w-xl mx-auto rounded-md wrapper text-white h-199 shadow-lg z-10 backdrop-filter backdrop-blur-sm">
-        <h1 className="text-xl font-bold">
+      <div className="ml-24 md:ml-1 lg:ml-20">
+        <img src={logoUrl} alt="logo img" className="w-48 md:w-24 lg:w-40" />
+      </div>
+      <div className=" p-4 -mt-24 lg:-mt-8 max-w-xl mx-auto rounded-md wrapper text-white h-199 shadow-lg z-10 backdrop-filter backdrop-blur-sm md:mt-2">
+        <h1 className="text-xl font-bold ">
           Create an account to continue to MovieFlex{" "}
         </h1>
-        <form className="ml-20 mr-20 mt-4 sm:ml-2">
+        <form className="ml-20 mr-20 mt-4">
           <label className="block mt-6">
             <div className="text-left">
               Name
-              <span className="text-primaryRed">*</span>
+              <span className="text-primaryRed ml-1">*</span>
             </div>
             <input
               type="text"
@@ -25,7 +29,7 @@ const SignUp = () => {
           <label className="block mt-6">
             <div className="text-left">
               Email address
-              <span className="text-primaryRed">*</span>
+              <span className="text-primaryRed ml-1">*</span>
             </div>
             <input
               type="email"
@@ -36,7 +40,7 @@ const SignUp = () => {
           <label className="block mt-6">
             <div className="text-left">
               Password
-              <span className="text-primaryRed">*</span>
+              <span className="text-primaryRed ml-1">*</span>
             </div>
             <div className="relative">
               <input
@@ -45,7 +49,7 @@ const SignUp = () => {
                 className="sign-border  mt-2 py-2  px-4 bg-transparent rounded-sm block w-full"
               />
               <span className="absolute inset-y-0 right-0 pr-3 flex items-center cursor-pointer">
-                <img src={eyeIcon} alt="eye icon" className="w-4 h-4 " />
+                <img src={eyeIconUrl} alt="eye icon" className="w-4 h-4 " />
               </span>
             </div>
           </label>
@@ -58,13 +62,19 @@ const SignUp = () => {
           <span className="text-SignUpColor">Sign In</span>
         </h4>
         <div className="mt-4 mb-4">OR</div>
-        <div className="flex items-center justify-center mb-2">
-          <img src={GIcon} alt="Google Icon" className="h-6 w-6 mr-2" />
-          <p>Continue with Google</p>
-        </div>
-        <div className="flex items-center justify-center mt-8">
-          <img src={Facebook} alt="Google Icon" className="ml-5 h-6 w-6 mr-2" />
-          <p>Continue with Facebook</p>
+        <div className="flex justify-center gap-3 mb-12">
+          <div className="flex items-center justify-center w-1/2 sign-border px-2 py-2">
+            <img src={GIconUrl} alt="Google Icon" className="h-6 w-6 mr-2" />
+            <p>Continue with Google</p>
+          </div>
+          <div className="flex items-center justify-center w-1/2 py-2 px-2 sign-border">
+            <img
+              src={FbIconUrl}
+              alt="Google Icon"
+              className="ml-5 h-6 w-6 mr-2"
+            />
+            <p>Continue with Facebook</p>
+          </div>
         </div>
       </div>
     </>
--- movie-flex-frontend/tailwind.config.js
+++ movie-flex-frontend/tailwind.config.js
@@ -3,10 +3,10 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     screens: {
-      // sm: "480px",
-      // md: "768px",
-      // lg: "976px",
-      // xl: "1440px",
+      sm: "480px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1440px",
     },
     extend: {
       colors: {
