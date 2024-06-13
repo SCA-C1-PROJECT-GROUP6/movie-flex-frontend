c9781541dfb69ef7fee42f64f16fe811c9a551f2
Adepoju Ronke <adepojuronkemercy@gmail.com>
2024-06-13 17:14:27 +0100

    worked on the footer

--- movie-flex-frontend/src/components/Navbar.jsx
+++ movie-flex-frontend/src/components/Navbar.jsx
@@ -2,37 +2,38 @@
 import Logo from "/logo.svg";
 import SignIn from "../pages/SignIn.jsx"
 const TopNavbar = () => {
-  return (
-    <nav className=" flex">
-      <div className="">
-        <img src={Logo} alt="logo" className="" />
-      </div>
+    return (
+        <nav className=" flex">
+            <div className="">
+                <a href="">
+                    <img src={Logo} alt="logo" className="" />
+                </a>
+            </div>
+            <div className="ml-auto flex items-center gap-20">
+                <div className="relative flex items-center w-full">
+                    <img
+                        src="/search.svg"
+                        alt="Search Icon"
+                        className="w-5 h-3 absolute inset-y-1/2 left-3 -translate-y-1/2"
+                    />
+                    <input
+                        type="text"
+                        placeholder="Search movie"
+                        className="bg-inherit text-white px-3 py-2 border border-red-600 rounded-full  outline-none focus:ring-1 focus:ring-red-600 pl-10 w-full"
+                    />
+                    <img
+                        src="/asterisk.svg"
+                        alt="Asterisk Icon"
+                        className="w-3 h-3 absolute inset-y-1/2 right-3 -translate-y-1/2"
+                    />
+                </div>
 
-      <div className="ml-auto flex items-center gap-20"> 
-        <div className="relative flex items-center w-full">
-          <img
-            src="/search.svg"
-            alt="Search Icon"
-            className="w-5 h-3 absolute inset-y-1/2 left-3 -translate-y-1/2"
-          />
-          <input
-            type="text"
-            placeholder="Search movie"
-            className="bg-inherit text-white px-3 py-2 border border-red-600 rounded-full  outline-none focus:ring-1 focus:ring-red-600 pl-10 w-full"
-          />
-          <img
-            src="/asterisk.svg"
-            alt="Asterisk Icon"
-            className="w-3 h-3 absolute inset-y-1/2 right-3 -translate-y-1/2"
-          />
-        </div>
-
-        <a href={SignIn} className="flex items-center px-5 py-2 rounded-md hover:bg-red-900 text-white whitespace-nowrap">
-          Sign In
-        </a>
-      </div>
-    </nav>
-  );
+                <a href={SignIn} className="flex items-center px-5 py-2 rounded-md hover:bg-red-900 text-white whitespace-nowrap">
+                    Sign In
+                </a>
+            </div>
+        </nav>
+    );
 };
 
 export default TopNavbar;
--- movie-flex-frontend/src/pages/SingUp.jsx
+++ movie-flex-frontend/src/pages/SingUp.jsx
@@ -2,14 +2,14 @@ import { useState } from "react";
 import SignUpValidator from "../validators/SignUpValidator";
 import "./SignUp.css";
 import TrailerButton from "../components/WatchTrailerBtn";
-
+import Footer from "../components/footer"
 
 
 
 const SignUp = () => {
 
   const movieUrl = "https://www.youtube.com/watch?v=Al4LG2YUa3k";
-  
+
   const eyeIconUrl = "https://i.imgur.com/nW2iNQm.png";
   const GIconUrl = "https://i.imgur.com/GJnmeaj.png";
   const FbIconUrl = "https://i.imgur.com/QFwv2qR.png";
@@ -53,8 +53,8 @@ const SignUp = () => {
 
   return (
     <>
-        <TrailerButton url={movieUrl} />
-        <div className="ml-24 md:ml-1 lg:ml-20">
+      <TrailerButton url={movieUrl} />
+      <div className="ml-24 md:ml-1 lg:ml-20">
         <img src={logoUrl} alt="logo img" className="w-48 md:w-24 lg:w-40 -mt-6" />
       </div>
       <div className=" p-4 -mt-24 lg:-mt-8 max-w-xl mx-auto rounded-md wrapper text-white h-199 shadow-lg z-10 backdrop-filter backdrop-blur-sm md:mt-2">
@@ -151,6 +151,7 @@ const SignUp = () => {
           </div>
         </div>
       </div>
+      <Footer />
     </>
   );
 };
