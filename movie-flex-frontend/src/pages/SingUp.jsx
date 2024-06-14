183e1cf16160406b6b52a2d2abd1bb9277c39324
Dabi-ayu <dabiayu8@gmail.com>
2024-06-14 20:42:50 +0100

    modifications

--- movie-flex-frontend/src/pages/SingUp.jsx
+++ movie-flex-frontend/src/pages/SingUp.jsx
@@ -4,7 +4,6 @@ import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 import axios from "../utils/axiosInstanc";
-import { Link } from "react-router-dom";
 
 import { toast } from "react-toastify";
 import "./SignUp.css";
