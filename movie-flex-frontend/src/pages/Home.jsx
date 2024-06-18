ede9fa55e9418b3fbbe09057dcddee00ffa6aee9
Dabi-ayu <dabiayu8@gmail.com>
2024-06-18 13:07:25 +0100

    fixes

---
+++ movie-flex-frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import NewOnMF from "../components/NewOnMF";
+import Footer from "../components/footer";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <>
+      <NewOnMF />
+      <Footer />
+    </>
+  );
+};
+export default Home;
