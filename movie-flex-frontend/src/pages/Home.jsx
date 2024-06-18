62f584c3db81906ee26943d85e32b77807e38073
Dabi-ayu <dabiayu8@gmail.com>
2024-06-18 12:56:25 +0100

    fixed bug

--- movie-flex-frontend/src/pages/Home.jsx
+++ movie-flex-frontend/src/pages/Home.jsx
@@ -1,13 +1,10 @@
 import NewOnMF from "../components/NewOnMF";
-import Footer from "../components/Footer";
-
 import "./Home.css";
 
 const Home = () => {
   return (
     <>
       <NewOnMF />
-      <Footer />
     </>
   );
 };
